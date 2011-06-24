4d4219016550739bc45462ef5576a9d5737c4e0e
valer <devnull@localhost>
2011-06-24 16:53:12 +0200

    Extra function headers declaration.

--- src/main/java/org/brackit/as/xquery/ASXQuery.java
+++ src/main/java/org/brackit/as/xquery/ASXQuery.java
@@ -27,27 +27,105 @@
  */
 package org.brackit.as.xquery;
 
+import org.brackit.as.xquery.function.bit.DeleteFile;
+import org.brackit.as.xquery.function.bit.Eval;
+import org.brackit.as.xquery.function.bit.LoadFile;
+import org.brackit.as.xquery.function.bit.MakeDirectory;
+import org.brackit.as.xquery.function.bit.StoreFile;
+import org.brackit.as.xquery.function.http.GetSessionAtt;
+import org.brackit.as.xquery.function.http.RemoveSessionAtt;
+import org.brackit.as.xquery.function.http.SetSessionAtt;
+import org.brackit.as.xquery.function.util.Template;
 import org.brackit.server.metadata.manager.MetaDataMgr;
 import org.brackit.server.xquery.compiler.DBCompiler;
 import org.brackit.server.xquery.optimizer.DBOptimizer;
 import org.brackit.xquery.QueryException;
 import org.brackit.xquery.XQuery;
+import org.brackit.xquery.atomic.QNm;
 import org.brackit.xquery.compiler.parser.ANTLRParser;
+import org.brackit.xquery.function.Signature;
 import org.brackit.xquery.module.Functions;
+import org.brackit.xquery.module.Namespaces;
+import org.brackit.xquery.sequence.type.AnyItemType;
+import org.brackit.xquery.sequence.type.AtomicType;
+import org.brackit.xquery.sequence.type.Cardinality;
+import org.brackit.xquery.sequence.type.SequenceType;
 
 /**
  * @author Sebastian Baechle
- *
+ * 
  */
 public class ASXQuery extends XQuery {
-	
+
 	static {
-		//Functions.predefine(function);
-		// TODO register db-specific functions
-		// Example:
-		// Functions.predefine(new GetSession());
+
+		// Bit
+		Functions.predefine(new DeleteFile(new QNm(Namespaces.BIT_NSURI,
+				Namespaces.BIT_PREFIX, "deleteFile"), new Signature(
+				new SequenceType(AtomicType.BOOL, Cardinality.One),
+				new SequenceType(AtomicType.STR, Cardinality.One))));
+
+		Functions.predefine(new Eval(new QNm(Namespaces.BIT_NSURI,
+				Namespaces.BIT_PREFIX, "eval"), new Signature(new SequenceType(
+				AtomicType.STR, Cardinality.ZeroOrOne), // result
+				new SequenceType(AtomicType.STR, Cardinality.One)))); // query
+
+		Functions.predefine(new LoadFile(new QNm(Namespaces.BIT_NSURI,
+				Namespaces.BIT_PREFIX, "loadFile"), new Signature(
+				// file as string result
+				new SequenceType(AtomicType.STR, Cardinality.ZeroOrOne),
+				new SequenceType(AtomicType.STR, Cardinality.One)))); // pathname
+
+		Functions.predefine(new MakeDirectory(new QNm(Namespaces.BIT_NSURI,
+				Namespaces.BIT_PREFIX, "makeDirectory"), new Signature(
+				// true: dir created, false otherwise
+				new SequenceType(AtomicType.STR, Cardinality.One),
+				new SequenceType(AtomicType.STR, Cardinality.One)))); // pathname
+
+		Functions.predefine(new StoreFile(new QNm(Namespaces.BIT_NSURI,
+				Namespaces.BIT_PREFIX, "storeFile"), new Signature(
+				new SequenceType(AtomicType.STR, Cardinality.ZeroOrOne),
+				new SequenceType(AtomicType.STR, Cardinality.One), // doc name
+				new SequenceType(AnyItemType.ANY, Cardinality.One))));
+
+		// HTTP
+		Functions.predefine(new SetSessionAtt(new QNm(Namespaces.BIT_NSURI,
+				Namespaces.HTTP_PREFIX, "setSessionAtt"), new Signature(
+				// output: true OK or exception
+				new SequenceType(AtomicType.BOOL, Cardinality.One),
+				new SequenceType(AtomicType.STR, Cardinality.One), // att name
+				new SequenceType(AnyItemType.ANY, Cardinality.One))));// attribute
+
+		Functions.predefine(new GetSessionAtt(new QNm(Namespaces.BIT_NSURI,
+				Namespaces.HTTP_PREFIX, "getSessionAtt"), new Signature(
+				// output: true OK or exception
+				new SequenceType(AnyItemType.ANY, Cardinality.One),
+				new SequenceType(AtomicType.STR, Cardinality.One)))); // att
+																		// name
+
+		Functions.predefine(new RemoveSessionAtt(new QNm(Namespaces.BIT_NSURI,
+				Namespaces.HTTP_PREFIX, "removeSessionAtt"), new Signature(
+				// output: true OK or exception
+				new SequenceType(AnyItemType.ANY, Cardinality.One),
+				new SequenceType(AtomicType.STR, Cardinality.One)))); // att
+																		// name
+
+		// Util
+		Functions.predefine(new Template(new QNm(Namespaces.BIT_NSURI,
+				Namespaces.UTIL_PREFIX, "template"), new Signature(
+				// Result output template page
+				new SequenceType(AnyItemType.ANY, Cardinality.One),
+				new SequenceType(AnyItemType.ANY, Cardinality.ZeroOrOne), // head
+				new SequenceType(AnyItemType.ANY, Cardinality.ZeroOrOne), // header
+				new SequenceType(AnyItemType.ANY, Cardinality.ZeroOrOne), // menu
+				new SequenceType(AnyItemType.ANY, Cardinality.ZeroOrOne), // content
+				new SequenceType(AnyItemType.ANY, Cardinality.ZeroOrOne)))); // footer
+
+		Functions.predefine(new Template(new QNm(Namespaces.BIT_NSURI,
+				Namespaces.UTIL_PREFIX, "template"), new Signature(
+				new SequenceType(AnyItemType.ANY, Cardinality.One), // result
+				new SequenceType(AnyItemType.ANY, Cardinality.One)))); // content
 	}
-	
 
 	public ASXQuery(String query, MetaDataMgr mdm) throws QueryException {
 		super(query, new ANTLRParser(), new DBOptimizer(mdm), new DBCompiler());
